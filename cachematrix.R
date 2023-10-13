77993f8519d1f2cb9309c6e2b54dafed11876eb0
Pritish Priyam <103728598+PritishPriyam@users.noreply.github.com>
2023-10-13 21:31:34 +0530

    Update cachematrix.R
    Comments added for description

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,11 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+#
 
+## Write a short comment describing this function
+## We've included the functions to set and get the values of 
+## the matrix.
 makeCacheMatrix <- function(x = matrix()) {
         inv_mat <- NULL
         set <- function(y) {
@@ -20,7 +23,10 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
-
+## What this function does is that it ensures that if
+## we have already calculated inverse, then we 
+## do not calculate it again and cache it. 
+## If not calculated before, then we calculate the inverse and store it for displaying later on.
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
         inv_mat <- x$getInverse()
